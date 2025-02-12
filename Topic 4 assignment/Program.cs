d680fd7c616a2d2060653ab768a0a90b16b5be3b
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-12 14:18:34 -0500

    done part one

--- Topic 4 assignment/Program.cs
+++ Topic 4 assignment/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Kylie Banman
+
+            //variables
+            string name, age, pay;
+
+            //Part one
+            Console.WriteLine("This is part one of the assignment");
+            Console.WriteLine("Hello what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine("Hi, " + name + "! How old are you?");
+            age = Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine("So you're " + age + "? That's not old at all!");
+            Console.WriteLine("How much do you make?");
+            pay = Console.ReadLine();
+            Console.WriteLine("$" + pay + "! I hope that's per hour and not per year!");
+
+
         }
     }
 }
