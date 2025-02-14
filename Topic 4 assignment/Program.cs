bf58288073fe39ed66a1b2ed447113b3d8f69e7a
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-13 19:40:17 -0500

    added part five

--- Topic 4 assignment/Program.cs
+++ Topic 4 assignment/Program.cs
@@ -7,9 +7,10 @@
             //Kylie Banman
 
             //variables
-            string name, age, pay, firstName, lastName, login, partThreeName;
+            string name, age, pay, firstName, lastName, login, partThreeName, itemOne, itemTwo;
             int grade, studentID, partThreeAge;
-            double average, numberOne, numberTwo, numberThree;
+            double average, numberOne, numberTwo, numberThree, priceOne, priceTwo, totalPrice, discount, amountOff, discountPrice;
+            double roundedDiscountPrice;
             decimal roundedAverage;
 
             //Part one
@@ -66,18 +67,39 @@
             //Console.WriteLine("And five years ago you were " + (partThreeAge - 5));
 
             ////Part four
-            Console.WriteLine("This is part four of the assignment");
-            Console.WriteLine("Hello. Input a number");
-            Double.TryParse(Console.ReadLine(), out numberOne);
-            Console.WriteLine(" ");
-            Console.WriteLine("Input another number");
-            Double.TryParse(Console.ReadLine(), out numberTwo);
-            Console.WriteLine(" ");
-            Console.WriteLine("Input one more number");
-            Double.TryParse(Console.ReadLine(), out numberThree);
-            Console.WriteLine(" ");
-            Console.WriteLine("If those numbers were added and then divided by two, it would equal " + ((numberOne + numberTwo + numberThree) / 2));
+            //Console.WriteLine("This is part four of the assignment");
+            //Console.WriteLine("Hello. Input a number");
+            //Double.TryParse(Console.ReadLine(), out numberOne);
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Input another number");
+            //Double.TryParse(Console.ReadLine(), out numberTwo);
+            //Console.WriteLine(" ");
+            //Console.WriteLine("Input one more number");
+            //Double.TryParse(Console.ReadLine(), out numberThree);
+            //Console.WriteLine(" ");
+            //Console.WriteLine("If those numbers were added and then divided by two, it would equal " + ((numberOne + numberTwo + numberThree) / 2));
 
+            ////Part five
+            Console.WriteLine("Please input the name of an item");
+            itemOne = Console.ReadLine();
+            Console.WriteLine("Please input the price in dollars of " + itemOne);
+            Double.TryParse(Console.ReadLine(), out priceOne);
+            Console.WriteLine(" ");
+            Console.WriteLine("Please input the name of another item");
+            itemTwo = Console.ReadLine();
+            Console.WriteLine("Please input the price in dollars of " + itemTwo);
+            Double.TryParse(Console.ReadLine(), out priceTwo);
+            totalPrice = priceOne + priceTwo;
+            Console.WriteLine(" ");
+            Console.WriteLine("The total price is " + totalPrice);
+            Console.WriteLine(" ");
+            Console.WriteLine("What percent discount is available?");
+            Double.TryParse(Console.ReadLine(), out discount);
+            amountOff = totalPrice * (discount/100);
+            discountPrice = totalPrice - amountOff;
+            Console.WriteLine(" ");
+            roundedDiscountPrice = (double)Decimal.Round((decimal)discountPrice, 2);
+            Console.WriteLine("The discounted price is " + roundedDiscountPrice);
 
 
             /*This readLine is here so the program doesn't instantly close when I run it on my laptop
